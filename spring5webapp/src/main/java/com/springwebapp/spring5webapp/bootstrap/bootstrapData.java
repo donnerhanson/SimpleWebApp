47fc35009a764014449a9e8557b10ab3f357d954
donnerhanson <donnerhanson@yahoo.com>
2021-04-09 23:50:55 -0700

    Update the example with publisher pojo + repository and added to the bootstrap

--- spring5webapp/src/main/java/com/springwebapp/spring5webapp/bootstrap/bootstrapData.java
+++ spring5webapp/src/main/java/com/springwebapp/spring5webapp/bootstrap/bootstrapData.java
@@ -2,8 +2,11 @@ package com.springwebapp.spring5webapp.bootstrap;
 
 import com.springwebapp.spring5webapp.domain.Author;
 import com.springwebapp.spring5webapp.domain.Book;
+import com.springwebapp.spring5webapp.domain.Publisher;
 import com.springwebapp.spring5webapp.repositories.AuthorRepository;
 import com.springwebapp.spring5webapp.repositories.BookRepository;
+import com.springwebapp.spring5webapp.repositories.PublisherRepository;
+import org.apache.tomcat.jni.Address;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 // tells spring to detect this
@@ -12,10 +15,13 @@ import org.springframework.stereotype.Component;
 public class bootstrapData implements CommandLineRunner {
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public bootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public bootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,6 +31,15 @@ public class bootstrapData implements CommandLineRunner {
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+
+        String name;
+        String addressLineOne;
+        String city;
+        String state;
+        int zip;
+
+        Publisher publisher = new Publisher("Penguin", "testAddr 123432", "testCity", "Test State", 23123);
+        publisherRepository.save(publisher);
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
@@ -45,6 +60,7 @@ public class bootstrapData implements CommandLineRunner {
         System.out.println("Started in Bootstrap");
         System.out.println("Number of books: " + bookRepository.count());
         System.out.println("Number of authors: " + authorRepository.count());
+        System.out.println("Number of publishers: " + publisherRepository.count());
 
     }
 }
